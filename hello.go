412c7a2e555d7da5c36831c564e232d2e2e3118b
MaheshMLOPS <maheshn@atomstate.com>
2024-07-22 15:29:04 +0530

    initial commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	 var n='z';
+    fmt.Println(reflect.TypeOf(n));
+	for i:=0;i<5;i++{
+		fmt.Println("lalaland");fmt.Println("lalaland");
+	}
+}
