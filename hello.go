babf50e513704e2e10aee87c27a102363c3b9346
MaheshMLOPS <maheshn@atomstate.com>
2024-07-23 07:53:01 +0530

    second commit

--- hello.go
+++ hello.go
@@ -6,9 +6,42 @@ import (
 )
 
 func main() {
-	 var n='z';
-    fmt.Println(reflect.TypeOf(n));
-	for i:=0;i<5;i++{
-		fmt.Println("lalaland");fmt.Println("lalaland");
+	fmt.Println("Variable declaration")
+	// variable declaration
+	 var var1='z';
+	 // checking the type of the variable
+    fmt.Println(reflect.TypeOf(var1));
+	var2 := 1
+
+	//printing two variables in same println
+	fmt.Println("var2" , var2);
+	//checking the type of variable
+    fmt.Println(reflect.TypeOf(var2))
+	fmt.Println("\nLoops\nfor-loop");
+
+	/* looping 
+		statements */
+	var i = 0
+	//for loop
+	for i=0;i<5;i++{
+		fmt.Print("i-");fmt.Println(i);
 	}
+	s :=""
+	fmt.Println("\nwhile-loop")
+	//while loop wth sprintf
+	for i > var2{
+		s+= fmt.Sprintf("i is --- %d",i);
+		s+="\n"
+		i=i-1;
+	}
+	fmt.Print(s);
+
+	//functions
+	fmt.Println("\nFunctions")
+	fmt.Print(sum(2,3))
+
+}
+
+func sum(num1 int ,num2 int) int{
+	return num1+num2
 }
